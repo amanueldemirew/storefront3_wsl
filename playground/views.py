3d75f65fd8241f1014558dfbff4655d63d05ed59
Amanuel <amanuel.demirew@yahoo.com>
2024-05-08 20:07:28 +0300

    modify

--- playground/views.py
+++ playground/views.py
@@ -2,14 +2,19 @@ from django.shortcuts import render
 from django.utils.decorators import method_decorator
 from django.views.decorators.cache import cache_page
 from rest_framework.views import APIView
-
+import logging
 import requests
 
+logger = logging.getLogger(__name__)
+
 class HelloView(APIView):
-    @method_decorator(cache_page(5 * 60))
     def get(self, request):
-        response = requests.get('https://httpbin.org/delay/2')
-        data = response.json()
+        try:
+            logger.info('calling httpbin')
+            response = requests.get('https://httpbin.org/delay/2')
+            data = response.json()
+        except request.ConnectionError:
+            logger.critical('httpbin ')
         return render(request, 'hello.html', {'name': data})
 
 
